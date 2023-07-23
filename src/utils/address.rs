6ff15d81a29f7418954b666d8f5b9614fb0b51d0
malik672 <aremumalik01@gmail.com>
2023-07-23 21:02:44 +0100

    is_address

---
+++ src/utils/address.rs
@@ -0,0 +1,12 @@
+//! Various utilities for manipulating Ethereum related data.
+use regex::Regex;
+
+///check if the is the string is a valid address
+pub fn is_address(address: &str) -> bool {
+    let checker = Regex::new(r"^(0x)?[0-9a-fA-F]{40}$").unwrap();
+    checker.is_match(address)
+}
+
+pub fn is_contract(address: &str) -> bool {
+    
+}
